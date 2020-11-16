a71f93e6c3bd22ec2bfdaae3b13bd76e08ab2936
Matthew Lock <matthewwilliamlock@gmail.com>
2020-11-16 23:54:05 +0200

    Updated projects section

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // Imports
 const express = require('express')
+const hbs = require('hbs')
 const path = require('path')
 const e = require('express')
 const fs = require('fs')
@@ -7,6 +8,8 @@ const {sendEmail} = require("./emails/message")
 
 // Define paths
 const publicDirectory = path.join(__dirname,'../public')
+const viewsDirecory = path.join(__dirname,'../templates/views')
+const partialsDirecory = path.join(__dirname,'../templates/partials')
 
 // Instantiate the server
 const app = express()
@@ -14,8 +17,21 @@ const app = express()
 // Get port number
 const port = process.env.PORT || 3000 // Set by heroku, default to 3000 locally
 
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsDirecory)
+hbs.registerPartials(partialsDirecory)
+
 // Setup static directory to serve
 app.use(express.static(publicDirectory))
+
+// This will never be seen as when static server is set up
+app.get('',(req,res)=>{
+    res.render('index',{
+        // title:'Weather Application',
+        // name:'Matthew Lock'
+    })
+})
 
 app.post('/message',(req,res)=>{ 
 
@@ -54,6 +70,24 @@ app.get('/designreport',(req,res)=>{
     res.contentType("application/pdf");
     res.send(data);
 })
+
+app.get('/imareport',(req,res)=>{
+    const file= path.join(__dirname,'../public/pdfs/IMA_YODA_Final_Report.pdf')
+    console.log(file)
+    var data =fs.readFileSync(file);
+    res.contentType("application/pdf");
+    res.send(data);
+})
+
+app.get('/musicspeechreport',(req,res)=>{
+    const file= path.join(__dirname,'../public/pdfs/Speech_Music_Discrimination_LCKMAT002_GDFLAW001.pdf')
+    console.log(file)
+    var data =fs.readFileSync(file);
+    res.contentType("application/pdf");
+    res.send(data);
+})
+
+
 
 app.get('/cv',(req,res)=>{
     const file= path.join(__dirname,'../public/pdfs/cv.pdf')
