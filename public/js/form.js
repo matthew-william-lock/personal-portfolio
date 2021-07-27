630f3dc4f05d8c1d08684f12c03d191888be0699
Matthew Lock <ml@hangar49.com>
2021-07-27 22:23:36 +0200

    update to readme

--- public/js/form.js
+++ public/js/form.js
@@ -8,76 +8,90 @@ const message = document.querySelector("#message")
 // const message = document.querySelector("#message-1")
 // const forecastMessage = document.querySelector("#message-2")
 
-messageForm.addEventListener('submit',(e) =>{
+messageForm.addEventListener('submit', (e) => {
     e.preventDefault() // Prevent refresh
 
-    // Check all required info
+    var form = document.getElementById('contact-form');
+    if (!form.checkValidity()) {
+        // Create the temporary button, click and remove it
+        var tmpSubmit = document.createElement('button')
+        form.appendChild(tmpSubmit)
+        tmpSubmit.click()
+        form.removeChild(tmpSubmit)
 
-    if (!name.value){
-        name.placeholder="Please enter your name!"
-        $('#name').addClass('error');
-    }
+    } else {
+        // Form is valid, do whatever you like
 
-    if (!email.value){
-        email.placeholder="Please enter your email!"
-        $('#email').addClass('error');
-    }
+        // Check all required info
+        // if (!name.value) {
+        //     name.placeholder = "Please enter your name!"
+        //     $('#name').addClass('error');
+        // }
 
-    if (!subject.value){
-        subject.placeholder="Please enter your subject!"
-        $('#subject').addClass('error');
-    }
+        // if (!email.value) {
+        //     email.placeholder = "Please enter your email!"
+        //     $('#email').addClass('error');
+        // }
 
-    if (!message.value){
-        message.placeholder="Please enter a message!"
-        $('#message').addClass('error');
-    }
+        // if (!subject.value) {
+        //     subject.placeholder = "Please enter your subject!"
+        //     $('#subject').addClass('error');
+        // }
 
-    // Stop execution
-    if ( !name.value || !email.value || !subject.value || !message.value){
-        return null
-    }
+        // if (!message.value) {
+        //     message.placeholder = "Please enter a message!"
+        //     $('#message').addClass('error');
+        // }
 
-    const url = '/message?name=' + (name.value) +"&email=" + (email.value) +"&subject=" + (subject.value) +"&message=" + (message.value);
-    console.log(url)
+        // Stop execution
+        if (!name.value || !email.value || !subject.value || !message.value) {
+            return null
+        }
 
-    fetch(url,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    }).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error){
-                alert("Something went wrong the message was not delivered...")
-            } else{    
-                alert('Message Sent!')    
-                name.value=""
-                email.value=""
-                subject.value=""
-                message.value=""    
-            }
+        const url = '/message?name=' + (name.value) + "&email=" + (email.value) + "&subject=" + (subject.value) + "&message=" + (message.value);
+        console.log(url)
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((response) => {
+            response.json().then((data) => {
+                if (data.error) {
+                    alert("Something went wrong the message was not delivered...")
+                } else {
+                    alert('Message Sent!')
+                    name.value = ""
+                    email.value = ""
+                    subject.value = ""
+                    message.value = ""
+                }
+            })
         })
-    })
+
+    }
+
+
 
 })
 
-name.onclick = function(event) {
-    name.placeholder="Your Name*"
+name.onclick = function (event) {
+    name.placeholder = "Your Name*"
     $('#name').removeClass('error');
 }
 
-email.onclick = function(event) {
-    email.placeholder="Your Email*"
+email.onclick = function (event) {
+    email.placeholder = "Your Email*"
     $('#email').removeClass('error');
 }
 
-subject.onclick = function(event) {
-    subject.placeholder="Your Subject*"
+subject.onclick = function (event) {
+    subject.placeholder = "Your Subject*"
     $('#subject').removeClass('error');
 }
 
-message.onclick = function(event) {
-    message.placeholder="Your Message*"
+message.onclick = function (event) {
+    message.placeholder = "Your Message*"
     $('#message').removeClass('error');
 }
